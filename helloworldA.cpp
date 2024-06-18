d92395584c0cb765508a293cf9d4aab7b38506d9
Ian Shi <ianshi1111@gmail.com>
2024-06-18 11:03:08 -0700

    c version

--- helloworldA.cpp
+++ helloworldA.cpp
@@ -5,5 +5,6 @@ int main(){
     //cout<<"hello world"<<endl;
     //cout<<"hello world\n";
     std::cout<<"hello world"<<std::endl; //if we do not use namespace std above
+    std::cout<<"this is working"<<std::endl;
     return 0;
 }
