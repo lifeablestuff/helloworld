93c1b96d1e8c4a9436adb67a8a6b06c7b0c4a95b
Ian Shi <ianshi1111@gmail.com>
2024-06-18 10:46:34 -0700

    my first commit

---
+++ helloworldA.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+//using namespace std;
+
+int main(){
+    //cout<<"hello world"<<endl;
+    //cout<<"hello world\n";
+    std::cout<<"hello world"<<std::endl; //if we do not use namespace std above
+    return 0;
+}
