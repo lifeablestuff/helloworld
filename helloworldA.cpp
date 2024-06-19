63026eb078b1f033046b5e33be4cc57ec2ee4602
Ian Shi <ianshi1111@gmail.com>
2024-06-19 10:47:37 -0700

    this is a better message

--- helloworldA.cpp
+++ helloworldA.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 //using namespace std;
-
-int main(){
+//a;usdfhslweuahflkawejghlkjashfskdjlahflskdjahfkljh
     //cout<<"hello world"<<endl;
     //cout<<"hello world\n";
     std::cout<<"hello world"<<std::endl; //if we do not use namespace std above
